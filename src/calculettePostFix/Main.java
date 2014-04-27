beb65bafb5ea15aeab334650ba6402d59501ce5b
Darkham666 <thomas.picard666@gmail.com>
2014-04-27 12:09:28 +0200

    Test
    Ajout de méthodes pour tester la gestion de pile dans le main

--- src/calculettePostFix/Main.java
+++ src/calculettePostFix/Main.java
@@ -1,5 +1,25 @@
 package calculettePostFix;
 
-public class Main {
+import java.util.Scanner;
 
+import calculettePostFix.Duracell;
+
+public class Main {
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("Veuillez saisir un chiffre :");
+		double formule = scan.nextDouble();
+		System.out.println("Vous avez saisi : " + formule);
+		Duracell pile = new Duracell();
+		pile.estVide();
+		pile.ajoute(52D);
+		System.out.println("Voici la pile :" + pile);
+		pile.estVide();
+		pile.ajoute(42D);
+		pile.estVide();
+		pile.retire();
+		pile.retire();
+		pile.estVide();
+	}
 }
