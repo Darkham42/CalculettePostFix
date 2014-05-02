050f0a607edccc34dbec95d08e0875abbea22823
Darkham666 <thomas.picard666@gmail.com>
2014-05-02 20:55:12 +0200

    Exception
    Ajout de l'exception dans le main

--- src/calculettePostFix/Main.java
+++ src/calculettePostFix/Main.java
@@ -8,17 +8,35 @@ public class Main {
 	static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		System.out.println("Veuillez saisir un chiffre :");
-		double formule = scan.nextDouble();
-		System.out.println("Vous avez saisi : " + formule);
-		Duracell pile = new Duracell();
-		System.out.println(pile.estVide());
-		pile.ajoute(52D);
-		System.out.println(pile.estVide());
-		pile.ajoute(42D);
-		System.out.println(pile.estVide());
-		pile.retire();
-		pile.retire();
-		System.out.println(pile.estVide());
+		try {
+			double formule = Double.NaN;
+			do {
+				formule = getValue();
+			} while (Double.isNaN(formule));
+			System.out.println("Vous avez saisi : " + formule);
+			Duracell pile = new Duracell();
+			System.out.println(pile.estVide());
+			pile.ajoute(formule);
+			System.out.println(pile.estVide());
+			pile.ajoute(42D);
+			System.out.println(pile.estVide());
+			pile.retire();
+			pile.retire();
+			System.out.println(pile.estVide());
+		}
+		catch (Exception e) {
+            System.out.println("Exception inconnue : \n" + e.toString());
+        }
+	}
+		
+	private static Double getValue() {
+		try {
+			System.out.println("Veuillez saisir un chiffre :");
+			return scan.nextDouble();
+		}
+		catch (Exception e) {
+			System.out.println("Valeur invalide : " + scan.nextLine());
+			return Double.NaN;
+		}
 	}
 }
