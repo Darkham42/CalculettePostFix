37af330204988c0bcbe8d82f83c71aa98e85f3e3
Darkham666 <thomas.picard666@gmail.com>
2014-05-02 22:54:53 +0200

    Ajout
    Ajout du package unaire
    Ajout de l'interface INombre

---
+++ src/calculette/INombre.java
@@ -0,0 +1,25 @@
+package calculette;
+
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+public class INombre implements IElement{
+
+	public double calcule(IPile evaluations, IIdentifiants ids)
+			throws IllegalStateException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String toStringInfix(Stack<String> chaines) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void analyse(Stack<IElement> elements, IIdentifiants ids)
+			throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
